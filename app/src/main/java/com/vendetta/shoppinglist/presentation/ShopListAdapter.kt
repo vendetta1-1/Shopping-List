e4c3d1c4d317c21de43779e1823b775369477143
sasha <berezhalex90@gmail.com>
2024-07-22 11:14:09 +0300

    finish RV

--- app/src/main/java/com/vendetta/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/vendetta/shoppinglist/presentation/ShopListAdapter.kt
@@ -4,36 +4,59 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.vendetta.shoppinglist.R
 import com.vendetta.shoppinglist.domain.ShopItem
 
 class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
 
-    val list = listOf<ShopItem>()
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_disabled,parent,false)
+        val layout = when(viewType){
+            VIEW_TYPE_ENABLED-> R.layout.item_shop_enabled
+            VIEW_TYPE_DISABLED->R.layout.item_shop_disabled
+            else-> throw RuntimeException("Unknown ViewType:$viewType")
+        }
+        val view = LayoutInflater.from(parent.context).inflate(layout, parent, false)
         return ShopItemViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
-        val shopItem = list[position]
-        holder.tvName.text = shopItem.name
-        holder.tvCount.text = shopItem.count.toString()
-        holder.view.setOnLongClickListener {
-            true
+    override fun getItemViewType(position: Int): Int {
+        return if (shopList[position].enabled) {
+            VIEW_TYPE_ENABLED
+        } else {
+            VIEW_TYPE_DISABLED
         }
     }
 
-    override fun getItemCount(): Int {
-        return list.size
+    override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
+        val shopItem = shopList[position]
+        viewHolder.view.setOnLongClickListener {
+            true
+        }
+        viewHolder.tvName.text = shopItem.name
+        viewHolder.tvCount.text = shopItem.count.toString()
     }
 
-    class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view){
+
+    override fun getItemCount(): Int {
+        return shopList.size
+    }
+
+    class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val tvName: TextView = view.findViewById(R.id.tv_name)
         val tvCount: TextView = view.findViewById(R.id.tv_count)
 
+    }
+    companion object{
+        const val VIEW_TYPE_ENABLED = 1
+        const val VIEW_TYPE_DISABLED = 0
     }
 
 }
